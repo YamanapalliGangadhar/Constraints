81a56e3a8ca4cffbfd611599f6f77c6f70acd235
Gangadhar <166109168+YamanapalliGangadhar@users.noreply.github.com>
2026-02-04 22:24:06 +0530

    Update matrix.sv

--- matrix.sv
+++ matrix.sv
@@ -1,16 +1,13 @@
-// write a constraint that has to generate 3X3 matrix , diagnol elements has to print multiples of 3, upper triangle  has to print multiples of 9 and lower triangle has to print multiples of 5  
-
-
 class matrix;
 	rand int a[3][3];
-		
+		//constraint c0{a.unique();}
 		constraint c{
 						foreach (a[i,j])
 								{
-									a[i][j] inside{[2:100]};
-									 i=j -> a[i][j]==i%3;
-									 i<j -> a[i][j]==i%5;
-									 i>j -> a[i][j]==i%9;
+									a[i][j] inside{[2:50]};
+									 i==j -> a[i][j]%3==0;
+									 i<j -> a[i][j]%5==0;
+									 i>j -> a[i][j]%9==0;
 									}
 					}
 endclass
@@ -22,8 +19,8 @@ endclass
 		assert(obj.randomize());
 			foreach(obj.a[i,j])
 					begin
-						$write("%0d",obj.a[i][j]);
-						if(i==2)
+						$write("%10d",obj.a[i][j]);
+						if(j==2)
 						$display("");
 					end
 			end
