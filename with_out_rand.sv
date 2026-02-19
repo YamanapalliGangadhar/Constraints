9af7766ea88b8b6998ddb7a8912376e9bcc6ae2b
Gangadhar <166109168+YamanapalliGangadhar@users.noreply.github.com>
2026-02-19 23:43:54 +0530

    Modify constraints and randomization in with_out_rand.sv
    Comment out the constraint and randomization logic for testing.

--- with_out_rand.sv
+++ with_out_rand.sv
@@ -1,12 +1,13 @@
 class four;
 	 bit[6:0] a;
-	 constraint c{ soft a>15;}
+	// constraint c{ soft a>15;}
 endclass
 module test;
 	four obj;
 	initial begin
 		obj=new(); repeat(5)begin
 		std::randomize(obj.a) with {obj.a<15;};
+		//std::randomize(obj.a);	
 		$display("%0d",obj.a);
 	end end
 endmodule 
